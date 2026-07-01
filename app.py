5b5b2aa555d4b9480ba078d0a7ddf39a902b454a
manikantab798-stack <manikantab798@gmail.com>
2026-07-01 17:14:35 +0530

    Update app.py
    Updated home function to include cooking trends data.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
-
+    chart_data = get_cooking_trends() 
+    return render_template('index.html', data=chart_data)
 if __name__ == "__main__":
     app.run(debug=True)
